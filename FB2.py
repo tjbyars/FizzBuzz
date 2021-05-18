a26fec9fb078fc7347bea86377c295116d1a3a1a
T J Byars <tombyars1@gmail.com>
2021-05-18 17:14:25 +0100

    Swap FB2 and FB3
    FB3 code was in FB2 file, swapped around so this code is now in FB3

--- FB2.py
+++ FB2.py
@@ -1,31 +1,3 @@
 # FizzBuzz 2
 # Run file and outputs to file
-# User specifed numbers and phrases and number counted up to
-
-class Info:
-    def __init__(self):
-        self.numStart = 0
-        self.numEnd = 0
-        self.step = 1
-    def setup(self):
-        self.numStart = int(input("Input the number you would like to count from\n"))
-        self.numEnd = int(input("Input the number you would like to count to\n"))
-        if self.numStart < self.numEnd:
-            step = -1
-        if self.numStart == self.numEnd:
-            setup()
-
-info = Info()
-info.setup() # Is true if increasing, false if decreasing
-print(info.numStart, "to", info.numEnd, "step", info.step)
-
-for i in range(info.numStart, info.numEnd, info.step):
-    outString = ""
-    if i % 3 == 0:
-        outString += "Fizz"
-    if i % 5 == 0:
-        outString += "Buzz"
-    if len(outString) == 0:
-        print(i)
-    else:
-        print(outString)
+# Runs through 1 to 100
---
+++ FB3.py
@@ -0,0 +1,31 @@
+# FizzBuzz 3
+# Run file and outputs to shell
+# User specifed numbers and phrases and number counted up to
+
+class Info:
+    def __init__(self):
+        self.numStart = 0
+        self.numEnd = 0
+        self.step = 1
+    def setup(self):
+        self.numStart = int(input("Input the number you would like to count from\n"))
+        self.numEnd = int(input("Input the number you would like to count to\n"))
+        if self.numStart < self.numEnd:
+            step = -1
+        if self.numStart == self.numEnd:
+            setup()
+
+info = Info()
+info.setup() # Is true if increasing, false if decreasing
+print(info.numStart, "to", info.numEnd, "step", info.step)
+
+for i in range(info.numStart, info.numEnd, info.step):
+    outString = ""
+    if i % 3 == 0:
+        outString += "Fizz"
+    if i % 5 == 0:
+        outString += "Buzz"
+    if len(outString) == 0:
+        print(i)
+    else:
+        print(outString)
