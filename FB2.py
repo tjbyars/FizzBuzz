6d0769ac6bdd02086cd7c742be35a0836368d010
T J Byars <tombyars1@gmail.com>
2021-05-18 21:59:35 +0100

    FB3 progress
    Minor FB2 correction
    FB3: Moved relation creation inside info class, not functioning yet, must add to relationList

--- FB2.py
+++ FB2.py
@@ -1,3 +1,18 @@
 # FizzBuzz 2
-# Run file and outputs to file
+# Run file and outputs to file, overwriting with standard FizzBuzz output
 # Runs through 1 to 100
+
+f = open("FizzBuzz.txt", "w")
+for i in range(1, 101):
+    out = ""
+    if (i % 3 == 0):
+        out += "Fizz"
+    if (i % 5 == 0):
+        out += "Buzz"
+    if len(out) == 0:
+        out = str(i)
+    # print to file
+    out += "\n"
+    f.write(out)
+
+f.close()
