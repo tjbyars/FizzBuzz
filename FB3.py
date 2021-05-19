2da4474c5461d3c7287957c6adc23deafb6b142f
T J Byars <tombyars1@gmail.com>
2021-05-19 23:57:08 +0100

    Input rework progress
    Restructuring input

--- FB3.py
+++ FB3.py
@@ -7,18 +7,30 @@ class Info:
         self.numStart = 0
         self.numEnd = 0
         self.step = 1
-        self.relationList = [[3, "Fizz"], [5, "Buzz"]]
+        self.relationList = []
+        self.count = 0
+        #[[3, "Fizz"], [5, "Buzz"]]
     def relSetup(self):
         run = True
         while run == True:
-            inputRel = input("Input the number-phrase relations you would like in the format \n"
-                            '[3,"Fizz"] \n'
-                            "Once you have entered all the relations, press enter. If you wish to run the standard FizzBuzz game, press enter before entering any relations \n")
-            if len(inputRel) == 0:
-                run = False
+            print("start count: ", self.count)
+            if self.count == 0:
+                inputRel = input("Input the first number-phrase relation you would like in the format: \n"
+                                "3,Fizz \n"
+                                "Once you have input the relation, press enter. If you wish to run the standard FizzBuzz game, press enter before entering any relations \n")
             else:
+                inputRel = input("Enter the next number-phrase relation in the format: \n"
+                                 "3,Fizz \n")
+            if len(inputRel) == 0:
+                self.count = -5
+            if self.count >= 0:
+                print("sus")
+                self.relationList.append(list(inputRel.split(",")))
+                self.count += 1
                 self.relSetup()
-                return False
+            else:
+                run = False
+                
     def setup(self):
         self.numStart = int(input("Input the number you would like to count from\n"))
         self.numEnd = int(input("Input the number you would like to count to\n")) + 1
@@ -26,20 +38,17 @@ class Info:
             step = -1
         if self.numStart == self.numEnd:
             setup()
-        self.relSetup()
+        self.count = 0
+        while self.count >= 0:
+            self.relSetup()
 
 info = Info()
 info.setup()
-print(info.numStart, "to", info.numEnd, "step", info.step)
-print(info.relationList)
 
 for i in range(info.numStart, info.numEnd, info.step):
     outString = ""
     for relation in info.relationList:
-        #print(relation)
-        numStr = str(relation)
-        #print(numStr)
-        num = int(numStr)
+        num = int(relation[0])
         if i % num == 0:
             outString += relation[1]
     if len(outString) == 0:
