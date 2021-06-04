7a94bdf18af3a4bbd40f0a4176e457b22cfbb5e8
T J Byars <tombyars1@gmail.com>
2021-06-04 20:59:14 +0100

    FB4 corrections implemented, bugfixes and superfluous code removed

--- FB4.py
+++ FB4.py
@@ -8,16 +8,12 @@ class Info:
         self.numEnd = 0
         self.step = 1
         self.relationList = []
-        self.count = 0
     def relSetup(self):
-        if self.count == 0:
-            inputRel = input("Input the first number-phrase relation you would like in the format: \n"
-                            "3,Fizz \n"
+        if len(self.relationList) == 0:
+            inputRel = input("Input the first number-phrase relation you would like in the format: 3,Fizz \n"
                             "Once you have input the relation, press enter. If you wish to run the standard FizzBuzz game, press enter before entering any relations \n")
         else:
-            inputRel = input("Enter the next number-phrase relation in the format: \n"
-                             "3,Fizz \n")
-        self.count += 1
+            inputRel = input("Enter the next number-phrase relation in the format: 3,Fizz \n"
         return inputRel
     def setup(self):
         self.numStart = int(input("Input the number you would like to count from\n"))
@@ -30,12 +26,26 @@ class Info:
         run = True
         while run == True:
             nextInp = self.relSetup()
+            containsRel = False
             if len(nextInp) == 0:
-                if self.count == 1:
+                if len(self.relationList) == 0:
                     self.relationList = [[3,"Fizz"],[5,"Buzz"]]
                 run = False
             else:
-                self.relationList.append(list(nextInp.split(",")))
+                inpList = list(nextInp.split(","))
+                print("relationList: " , self.relationList)
+                print("Input: " , inpList)
+                if len(self.relationList) == 0:
+                    self.relationList.append(inpList)
+                else:
+                    for relation in self.relationList:
+                        if relation[0] == inpList[0]:
+                            containsRel = True
+                    if containsRel == True:
+                        relation[1] = inpList[1]
+                    else:
+                        self.relationList.append(inpList)
+
 
 info = Info()
 info.setup()
